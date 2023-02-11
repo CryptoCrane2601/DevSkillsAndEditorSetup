975529ffc59a83cfac190d5cb4e2889af6a64a67
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-02-11 11:37:32 +0100

    Learning do code

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+
+const xyz = 23;
+if (xyz === 23) console.log('23');
+
+const calcAge = birthYear => 2023 - birthYear;
+
+console.log(xyz);
+console.log(calcAge(1989));
+
+// LEC
+// TODO
+// BUG
